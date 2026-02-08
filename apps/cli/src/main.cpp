cd97baab2eb8014be89920d1020c72167a4cb8d0
Eugen Michaels <evm72011@gmail.com>
2026-02-08 12:24:16 +0100

    example

--- apps/cli/src/main.cpp
+++ apps/cli/src/main.cpp
@@ -1,49 +1,6 @@
 #include <iostream>
-#include <random>
-
-#include <fink/instruments/aliases.hpp>
-#include <fink/math/discount.hpp>
-
-#include <fink/models/gbm.hpp>
-
-#include <fink/pricers/black_scholes.hpp>
-#include <fink/pricers/european_mc.hpp>
-#include <fink/rng/normal_rng.hpp>
-#include <fink/rng/pcg32.hpp>
 
 int main()
 {
-    std::cout << std::boolalpha;
-
-    using namespace fink::instruments;
-
-    const european_call c{
-        .expiry = 1.5,
-        .payoff = call_payoff{.strike = 100.0},
-    };
-
     std::cout << "fink-cli:\n";
-    std::cout << (c.style == exercise_style::european) << '\n';
-    std::cout << c.payoff(110.0) << '\n';
-
-    std::cout << 100.0 * fink::math::discount_continuous(.05, 1) << '\n';
-
-    using namespace fink::models;
-    gbm_params params{.s0{100.0}, .r{0.05}, .sigma{0.1}};
-
-    std::random_device rd;
-    fink::rng::pcg32 r(rd());
-    fink::rng::normal_rng normal_rng(r);
-    auto z = normal_rng();
-
-    auto price_s = gbm_terminal_price(params, 1.0, z);
-    std::cout << z << " : " << price_s << '\n';
-
-    auto price_o1 =
-        fink::pricers::bs_european_call(100.0, 100.0, 0.05, 0.2, 1.5);
-    std::cout << price_o1 << '\n';
-
-    fink::pricers::mc_config conf;
-    auto price_o2 = fink::pricers::price_european_mc(c, params, conf);
-    std::cout << price_o2.price << '\n';
 }
--- apps/examples/european_call/src/main.cpp
+++ apps/examples/european_call/src/main.cpp
@@ -1,15 +1,80 @@
 #include <iostream>
+#include <random>
+#include <string_view>
+#include <iomanip>
+#include <format>
 
 #include <fink/instruments/aliases.hpp>
 #include <fink/models/gbm.hpp>
+#include <fink/rng/normal_rng.hpp>
+#include <fink/rng/pcg32.hpp>
+#include <fink/pricers/black_scholes.hpp>
+#include <fink/pricers/european_mc.hpp>
 
 using namespace fink::instruments;
 
+void print_row(std::string_view label, double value)
+{
+    constexpr int w = 50;
+    std::cout << std::left
+              << std::setfill('.') << std::setw(w) << label
+              << std::setfill(' ') << value << '\n';
+}
+
+std::string format_paths(size_t n)
+{
+    if (n >= 1'000'000)
+        return std::to_string(n / 1'000'000) + "M";
+    if (n >= 1'000)
+        return std::to_string(n / 1'000) + "K";
+    return std::to_string(n);
+}
+
 int main()
 {
-    const european_call option{
-        .expiry = 1.5,
-        .payoff = call_payoff{.strike = 120.0},
+    const auto spot = 100.0; 
+    const auto sigma = 0.1;
+    const auto r = 0.05;    
+    const auto T = 1.0;    
+
+    print_row("Spot price", spot);
+    print_row("Volatility (stddev)", sigma);
+    print_row("Risk free rate", r);
+    print_row("Time to maturity (years)", T);
+
+    std::random_device rd;
+    fink::rng::pcg32 pcg(rd());
+    fink::rng::normal_rng normal_rng(pcg);
+
+    fink::models::gbm_params gbm_params{
+        .s0{spot},
+        .r{r},
+        .sigma{sigma},
     };
-    std::cout << option.expiry << '\n';
+    auto z = normal_rng();
+    auto price_spot = fink::models::gbm_terminal_price(gbm_params, T, z);
+    std::cout << '\n';
+    print_row("Possible spot price", price_spot);
+
+    const auto strike = 120.0;
+    print_row("Strike price", strike);
+    std::cout << '\n';
+
+    auto price_bs = fink::pricers::bs_european_call(spot, strike, r, sigma, T);
+    print_row("Black-Sholes analytical price", price_bs);
+
+    const european_call option{
+        .expiry = T,
+        .payoff = call_payoff{.strike = strike},
+    };
+
+    size_t paths[] { 100'000, 1'000'000, 5'000'000, 10'000'000};
+    for (auto path : paths) {
+            fink::pricers::mc_config mc_config {
+                .paths=path,
+            };
+            auto price_mc_native = fink::pricers::price_european_mc(option, gbm_params, mc_config);
+            auto label = "Monte Carlo native price [" + format_paths(path) + " paths]:";
+            print_row(label, price_mc_native.price);
+    }
 }
