571c9240995eb690839cb78a4f3fd555f30a0696
Eugen Michaels <evm72011@gmail.com>
2026-02-13 13:17:05 +0100

    cuda win

--- cmake/EnableCuda.cmake
+++ cmake/EnableCuda.cmake
@@ -1,8 +1,13 @@
 include_guard(GLOBAL)
 
 if(ENABLE_CUDA)
+    set(CMAKE_CUDA_STANDARD 17)
+    set(CMAKE_CUDA_STANDARD_REQUIRED ON)
+    set(CMAKE_CUDA_EXTENSIONS OFF)
+
     find_package(CUDAToolkit REQUIRED)
     enable_language(CUDA)
+
     message(STATUS "CUDA enabled: ${CUDAToolkit_VERSION}")
 else()
     message(STATUS "CUDA disabled")
